e02f50fc08ea896fdc92b530ae4b07acf9be8305
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-04-16 06:11:49 +0300

    React component basics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+const products = [
+  {name: 'product1', price: 100.0},
+  {name: 'product2', price: 100.0},
+]
+
 function App() {
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
