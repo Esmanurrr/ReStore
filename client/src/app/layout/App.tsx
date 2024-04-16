9ff5ce70b94570e45456909af70e0a12714e8625
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-04-16 08:10:58 +0300

    Using material ui theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,39 +1,26 @@
-import { useEffect, useState } from "react"
-import { Product } from "../models/product";
 import Catalog from "../../features/catalog/catalog";
-import { Container, CssBaseline, Typography } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, Typography, createTheme } from "@mui/material";
 import Header from "./Header";
+import { useState } from "react";
 
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
-
-  function addProduct(){
-    setProducts(prevState => [...prevState, 
-      {
-        id:prevState.length + 101,
-        name: 'product' + (prevState.length +1), 
-        price: (prevState.length * 100) + 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200'
-      }])
-  }
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette:{
+      mode: paletteType
+    }
+  })
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog/>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
