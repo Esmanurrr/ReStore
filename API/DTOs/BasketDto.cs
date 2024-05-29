3d3a40f8bcd0a08d1eb4eb58f32a26d53de564ff
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-05-29 06:19:23 +0300

    Shaping the data to return

---
+++ API/DTOs/BasketDto.cs
@@ -0,0 +1,11 @@
+﻿using API.Entities;
+
+namespace API.DTOs
+{
+    public class BasketDto
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+    }
+}
