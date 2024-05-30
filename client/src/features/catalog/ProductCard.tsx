ba5d3e773eb16b920d175dff0e79c8cdf762927f
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-05-31 01:53:05 +0300

    Refactoring the app to use the redux store

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import { LoadingButton } from '@mui/lab';
 import { useStoreContext } from "../../app/context/StoreContext";
 import { currencyFormat } from "../../app/util/util";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setBasket } from "../basket/basketSlice";
 
 interface Props {
     product: Product;
@@ -13,11 +15,12 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
     const [loading, setLoading] = useState(false);
-    const {setBasket} = useStoreContext();
+    const dispatch = useAppDispatch();
+
 
     function handleAddItem(productId: number){
         setLoading(true);
-        agent.Basket.addItem(productId).then(basket => setBasket(basket)).catch(error => console.log(error)).finally(() => setLoading(false));
+        agent.Basket.addItem(productId).then(basket => dispatch(setBasket(basket))).catch(error => console.log(error)).finally(() => setLoading(false));
     }
     return (
         <Card>
