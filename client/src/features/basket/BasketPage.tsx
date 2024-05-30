34caa4f3e059d26f277fec2578c64996c17dee8e
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-05-30 20:01:07 +0300

    Adding the remove item functionality

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,9 +1,24 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Add, Delete, Remove } from "@mui/icons-material";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from "@mui/lab";
 
 export default function BasketPage() {
-    const {basket} = useStoreContext();
+    const {basket, setBasket, removeItem} = useStoreContext();
+    const [loading, setLoading] = useState(false);
+
+
+    function handleAddItem(productId: number){
+        setLoading(true);
+        agent.Basket.addItem(productId).then(basket => setBasket(basket)).catch(error => console.log(error)).finally(() => setLoading(false));
+    }
+
+    function handleRemoveItem(productId: number, quantity = 1){
+        setLoading(true);
+        agent.Basket.removeItem(productId, quantity).then(() => removeItem(productId, quantity)).catch(error => console.log(error)).finally(() => setLoading(false));
+    }
 
     if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
 
@@ -14,7 +29,7 @@ export default function BasketPage() {
                     <TableRow>
                         <TableCell>Product</TableCell>
                         <TableCell align="right">Price</TableCell>
-                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="center">Quantity</TableCell>
                         <TableCell align="right">Subtotal</TableCell>
                         <TableCell align="right"></TableCell>
                     </TableRow>
@@ -23,14 +38,28 @@ export default function BasketPage() {
                     {
                         basket.items.map(item => (
                             <TableRow key={item.productId} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell component="th" scope="row">{item.name}</TableCell>
+                                <TableCell component="th" scope="row">
+                                    <Box display='flex' alignItems='center'>
+                                        <img src={item.pictureUrl} alt={item.name} style={{height: 50, marginRight: 20}}/>
+                                        <span>{item.name}</span>
+                                    </Box>
+
+                                </TableCell>
                                 <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
-                                <TableCell align="right">{item.quantity}</TableCell>
+                                <TableCell align="center">
+                                    <LoadingButton loading={loading} onClick={() => handleRemoveItem(item.productId)} color="error">
+                                        <Remove />
+                                    </LoadingButton>
+                                    {item.quantity}
+                                    <LoadingButton loading={loading} onClick={() => handleAddItem(item.productId)} color="secondary">
+                                        <Add />
+                                    </LoadingButton>
+                                </TableCell>
                                 <TableCell align="right">${((item.price / 100) * item.quantity).toFixed(2)}</TableCell>
                                 <TableCell align="right">
-                                    <IconButton color="error">
+                                    <LoadingButton loading={loading} onClick={() => handleRemoveItem(item.productId, item.quantity)} color="error">
                                         <Delete />
-                                    </IconButton>
+                                    </LoadingButton>
                                 </TableCell>
 
                             </TableRow>
