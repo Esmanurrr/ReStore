1e9361f6b4106a37994f0a85d49b446bd1fb0d84
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-06-08 14:53:36 +0300

    added search functionality to the api

--- API/Extensions/ProductExtensions.cs
+++ API/Extensions/ProductExtensions.cs
@@ -5,7 +5,7 @@ namespace API.Extensions
     public static class ProductExtensions
     {
        public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
-        {
+       {
             if(string.IsNullOrEmpty(orderBy)) return query.OrderBy(p => p.Name);    
 
             query = orderBy switch
@@ -15,6 +15,15 @@ namespace API.Extensions
                 _ => query.OrderBy(p => p.Name)
             };
             return query;
+       }
+
+       public static IQueryable<Product> Search(this IQueryable<Product> query, string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm)) return query;
+
+            var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
+
+            return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
         }
     }
 }
