83199f3fd057c913371d894b12f055cd56e05325
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-06-08 15:46:46 +0300

    Creatied a class that extends our List into a PagedList

---
+++ API/RequestHelpers/PagedList.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.RequestHelpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            MetaData = new MetaData
+            {
+                TotalCount = count,
+                PageSize = pageSize, //item count per page
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize) //to get int number we use ceiling
+            };
+            AddRange(items);
+        }
+
+        public MetaData MetaData { get; set; }
+
+        public static async Task<PagedList<T>> ToPagedList(IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var count = await query.CountAsync(); // how many item 
+            var items = await query.Skip((pageNumber - 1)*pageSize).Take(pageSize).ToListAsync(); // retrieves items from the specified page
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
