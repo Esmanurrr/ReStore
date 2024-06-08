d1948a9501302cea44265518a956d69f301dc463
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-06-08 12:48:58 +0300

    Fixed

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,31 +1,26 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from '@mui/material';
 import { useState, useEffect, ChangeEvent } from 'react';
 import { useParams } from 'react-router-dom';
-import { Product } from '../../app/models/product';
-import agent from '../../app/api/agent';
 import NotFound from '../../app/errors/NotFound';
 import LoadingComponent from '../../app/layout/LoadingComponent';
-import { useStoreContext } from '../../app/context/StoreContext';
 import { LoadingButton } from '@mui/lab';
 import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
-import { addBasketItemAsync, removeBasketItemAsync, setBasket } from '../basket/basketSlice';
+import { addBasketItemAsync, removeBasketItemAsync } from '../basket/basketSlice';
+import { fetchProductAsync, productSelectors } from './catalogSlice';
 
 export default function ProductDetails() {
     const {basket, status} = useAppSelector(state => state.basket);
     const dispatch = useAppDispatch();
     const { id } = useParams<{ id: string }>();
-    const [product, setProduct] = useState<Product | null>(null);
-    const [loading, setLoading] = useState(true);
+    const product = useAppSelector(state => productSelectors.selectById(state, +id!));
+    const {status: productStatus} = useAppSelector(state => state.catalog);
     const [quantity, setQuantity] = useState(0);
     const item = basket?.items.find(i => i.productId === product?.id);
 
     useEffect(() => {
         if(item) setQuantity(item.quantity);
-        id && agent.Catalog.details(parseInt(id))
-            .then(response => setProduct(response))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-    }, [id, item]);
+        if(!product && id) dispatch(fetchProductAsync(parseInt(id)));
+    }, [id, item, dispatch, product]);
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>){
         if(parseInt(event.currentTarget.value) >= 0){
@@ -36,14 +31,14 @@ export default function ProductDetails() {
     function handleUpdateCart(){
         if(!item || quantity > item.quantity){
             const updatedQuantity = item ? quantity - item.quantity : quantity;
-            dispatch(addBasketItemAsync({productId: product?.id!, quantity: updatedQuantity}))
+            dispatch(addBasketItemAsync({productId: product?.id, quantity: updatedQuantity}))
         }else{
             const updatedQuantity = item.quantity - quantity;
-            dispatch(removeBasketItemAsync({productId: product?.id!, quantity: updatedQuantity}))
+            dispatch(removeBasketItemAsync({productId: product?.id, quantity: updatedQuantity}))
         }
     }
 
-    if (loading) return <LoadingComponent message='Loading product...' />
+    if (productStatus.includes('pending')) return <LoadingComponent message='Loading product...' />
 
     if (!product) return <NotFound />
 
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
-import { RoolState } from "../../app/store/configureStore";
+import { RootState } from "../../app/store/configureStore";
 
 const productsAdapter = createEntityAdapter<Product>();
 
@@ -14,7 +14,17 @@ export const fetchProductsAsync = createAsyncThunk<Product[]>(
             console.log(error);
         }
     }
+)
 
+export const fetchProductAsync = createAsyncThunk<Product, number>(
+    'catalog/fetchProductAsync',
+    async (productId) => {
+        try {
+            return await agent.Catalog.details(productId);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 )
 
 export const catalogSlice = createSlice({
@@ -26,17 +36,29 @@ export const catalogSlice = createSlice({
     reducers: {},
     extraReducers: (builder => {
         builder.addCase(fetchProductsAsync.pending, (state) => {
-            state.status = 'pendingFetchProducts';
+            state.status = 'pendingFetchProducts'
         });
         builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
             productsAdapter.setAll(state, action.payload);
-            state.status= 'idle';
+            state.status = 'idle',
             state.productsLoaded = true;
         });
-        builder.addCase(fetchProductsAsync.rejected, (state) => {
+        builder.addCase(fetchProductsAsync.rejected, (state, action) => {
+            console.log(action.payload);
             state.status = 'idle';
+        });
+        builder.addCase(fetchProductAsync.pending, (state) => {
+            state.status = 'pendingFetchProduct'
+        });
+        builder.addCase(fetchProductAsync.fulfilled, (state, action) => {
+            productsAdapter.upsertOne(state, action.payload);
+            state.status = 'idle'
+        });
+        builder.addCase(fetchProductAsync.rejected, (state, action) => {
+            console.log(action);
+            state.status = 'idle'
         })
     })
 })
 
-export const productSelectors = productsAdapter.getSelectors((state: RoolState) => state.catalog);
+export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
