b68ec28d5c745846388af23d45ad1e0b6dcfb479
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-06-10 15:13:39 +0300

    Added filtering to the client

--- client/src/features/catalog/catalog.tsx
+++ client/src/features/catalog/catalog.tsx
@@ -3,27 +3,28 @@ import ProductList from './ProductList';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
 import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from './catalogSlice';
-import { Box, Checkbox, FormControlLabel, FormGroup, Grid, Pagination, Paper,  Typography } from '@mui/material';
+import { Box, Grid, Pagination, Paper, Typography } from '@mui/material';
 import ProductSearch from './ProductSearch';
 import RadioButtonGroup from '../../app/components/RadioButtonGroup';
+import CheckboxButtons from '../../app/components/CheckboxButtons';
 
 const sortOptions = [
-    {value: 'name', name: 'Alphabetical'},
-    {value: 'priceDesc', name: 'Price - High to low'},
-    {value: 'price', name: 'Price - Low to high'},
+    { value: 'name', name: 'Alphabetical' },
+    { value: 'priceDesc', name: 'Price - High to low' },
+    { value: 'price', name: 'Price - Low to high' },
 ]
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status, filtersLoaded, brands, types, productParams } = useAppSelector(state => state.catalog);
+    const { productsLoaded, status, filtersLoaded, brands, types, productParams } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        if(!productsLoaded) dispatch(fetchProductsAsync());
+        if (!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch])
 
     useEffect(() => {
-        if(!filtersLoaded) dispatch(fetchFilters());
+        if (!filtersLoaded) dispatch(fetchFilters());
     }, [dispatch, filtersLoaded])
 
 
@@ -32,42 +33,38 @@ export default function Catalog() {
     return (
         <Grid container spacing={4}>
             <Grid item xs={3}>
-                <Paper sx={{mb: 2}}>
-                    <ProductSearch/>
+                <Paper sx={{ mb: 2 }}>
+                    <ProductSearch />
                 </Paper>
-                <Paper sx={{mb: 2, p: 2}}>
+                <Paper sx={{ mb: 2, p: 2 }}>
                     <RadioButtonGroup selectedValue={productParams.orderBy}
-                        options={sortOptions} onChange={(e) => dispatch(setProductParams({orderBy: e.target.value}))} />
+                        options={sortOptions} onChange={(e) => dispatch(setProductParams({ orderBy: e.target.value }))} />
                 </Paper>
-                <Paper sx={{p: 2, mb: 2}}>
-                    <FormGroup>
-                        {
-                            brands.map(brand => (
-                                <FormControlLabel control={<Checkbox/>} label={brand} key={brand} />
-                            ))
-                        }
-                    </FormGroup>
+                <Paper sx={{ p: 2, mb: 2 }}>
+                    <CheckboxButtons
+                        items={brands}
+                        checked={productParams.brands}
+                        onChange={(items: string[]) => dispatch(setProductParams({ brands: items }))}
+                    />
                 </Paper>
-                <Paper sx={{p: 2, mb: 2}}>
-                    <FormGroup>
-                        {
-                            types.map(type => (
-                                <FormControlLabel control={<Checkbox/>} label={type} key={type} />
-                            ))
-                        }
-                    </FormGroup>
+                <Paper sx={{ p: 2, mb: 2 }}>
+                    <CheckboxButtons
+                        items={types}
+                        checked={productParams.types}
+                        onChange={(items: string[]) => dispatch(setProductParams({ types: items }))}
+                    />
                 </Paper>
             </Grid>
             <Grid item xs={9}>
                 <ProductList products={products} />
             </Grid>
-            <Grid item xs={3}/>
+            <Grid item xs={3} />
             <Grid item xs={9}>
                 <Box display='flex' justifyContent='space-between' alignItems='center'>
                     <Typography>
                         Displaying 1-6 of 20 items
                     </Typography>
-                    <Pagination color='secondary' size='large' count={18} page={2}/>
+                    <Pagination color='secondary' size='large' count={18} page={2} />
                 </Box>
             </Grid>
         </Grid>
